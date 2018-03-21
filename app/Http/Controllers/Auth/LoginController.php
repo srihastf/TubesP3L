91baf9d95a12bb6545057229e11e191e9fc2e342
Fikri Nugraha <fikri.nugraha@studentpartner.com>
2018-03-22 01:39:31 +0700

    ini coba coba merge

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/home';    //jadi gini eaaks mas ini cuman coba coba buat merge se line yang sama
 
     /**
      * Create a new controller instance.
